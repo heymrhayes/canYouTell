19e609596bba6ec36d2c291e430daa79ca31381a
heymrhayes <heymrhayes@users.noreply.github.com>
2019-12-21 15:15:57 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,83 @@
+// modified from https://developers.google.com/web/fundamentals/primers/service-workers
+
+
+var CACHE_NAME = 'canYouTell-cache-v1';
+var urlsToCache = [
+  './',
+  './index.html',
+  './home.html',
+  './play.html',
+  './results.html',
+  './memberData.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v48/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+  'https://unpkg.com/material-components-web@4.0.0/dist/material-components-web.min.css',
+  'https://unpkg.com/dexie@2.0.4/dist/dexie.js',
+  'https://unpkg.com/material-components-web@4.0.0/dist/material-components-web.min.js'
+];
+
+
+// delete old caches on activate
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
